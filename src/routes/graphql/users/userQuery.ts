23c0449fde97dbb5bb1c654e315dd7eea9958e37
Oleg Semenov <semoleg1986@gmail.com>
2023-07-22 17:40:35 +0300

    feat: add userQuery

---
+++ src/routes/graphql/users/userQuery.ts
@@ -0,0 +1,24 @@
+import { GraphQLList, GraphQLObjectType } from 'graphql';
+import { UserType } from './typeUser.js';
+import { Context } from '../types/context.js';
+import { UUIDType } from '../types/uuid.js';
+import { User } from '@prisma/client';
+
+export const getUserQuery = {
+  user: {
+    type: UserType as GraphQLObjectType,
+    args: {
+      id: { type: UUIDType },
+    },
+    resolve: async (_: unknown, { id }: User, { prisma }: Context) =>
+      await prisma.user.findUnique({ where: { id } }),
+  },
+
+  users: {
+    type: new GraphQLList(UserType),
+    resolve: async (_: unknown, __: unknown, { prisma }: Context) => {
+      const users = await prisma.user.findMany();
+      return users;
+    },
+  },
+};
