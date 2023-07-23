275c05d08b1e3498f98a170ecf3f680f43b9d630
Oleg Semenov <semoleg1986@gmail.com>
2023-07-24 02:00:10 +0300

    fix: bugs

--- src/routes/graphql/post/postMutation.ts
+++ src/routes/graphql/post/postMutation.ts
@@ -27,13 +27,14 @@ export const PostMutation = {
             dto: {type: new GraphQLNonNull(CreatePostInput)},
         },
         resolve: async (__: unknown, {dto}: ICreatePost, {prisma}: Context) =>{
-            await prisma.post.create({ data: dto})},
+          return await prisma.post.create({ data: dto})
+        },
     },
     changePost: {
       type: PostType as GraphQLObjectType,
-      args: { id: { type: new GraphQLNonNull(UUIDType) }, dto: { type: ChangePostInput } },
+      args: { id: { type: new GraphQLNonNull(UUIDType) }, dto: { type:ChangePostInput} },
       resolve: async (__: unknown, {id, dto}:IChangePost, { prisma }: Context) =>{
-        await prisma.post.update({ where: { id }, data: dto })},
+        return await prisma.post.update({ where: { id }, data: dto })},
     },
     deletePost: {
       type: PostType,
@@ -41,10 +42,10 @@ export const PostMutation = {
       resolve: async (__: unknown, {id}:{id:string},{ prisma }: Context) =>{
         try {
           await prisma.post.delete({ where: { id } })
-          return id; 
         } catch {
-          return null;
+          return false;
         }
+        return true;
 },
     }
 };
