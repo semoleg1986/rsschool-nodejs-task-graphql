d63f4069d9fcb45c9df26136f30f7df38f03d503
Oleg Semenov <semoleg1986@gmail.com>
2023-07-24 17:43:55 +0300

    feat: add dataloaders

--- src/routes/graphql/users/userQuery.ts
+++ src/routes/graphql/users/userQuery.ts
@@ -1,8 +1,9 @@
-import { GraphQLList, GraphQLObjectType } from 'graphql';
+import { GraphQLList, GraphQLObjectType, GraphQLResolveInfo } from 'graphql';
 import { UserType } from './userType.js';
 import { Context } from '../types/context.js';
 import { UUIDType } from '../types/uuid.js';
 import { User } from '@prisma/client';
+import { ResolveTree, parseResolveInfo, simplifyParsedResolveInfoFragmentWithType } from 'graphql-parse-resolve-info';
 
 export const getUser = {
   user: {
@@ -10,14 +11,39 @@ export const getUser = {
     args: {
       id: { type: UUIDType },
     },
-    resolve: async (_: unknown, { id }: User, { prisma }: Context) =>
-      await prisma.user.findUnique({ where: { id } }),
+    resolve: async (_: unknown, { id }: User, { loaders }: Context) =>
+      await loaders.userDataLoader.load(id),
   },
 
   users: {
     type: new GraphQLList(UserType),
-    resolve: async (_: unknown, __: unknown, { prisma }: Context) => {
-      const users = await prisma.user.findMany();
+    resolve: async (
+      _: unknown,
+      __: unknown,
+      { prisma, loaders }: Context,
+      info: GraphQLResolveInfo,
+    ) => {
+      const parsedInfo = parseResolveInfo(info) as ResolveTree;
+      const returnType = info.returnType;
+
+      const { fields } = simplifyParsedResolveInfoFragmentWithType(
+        parsedInfo,
+        returnType,
+      );
+
+      const include = {};
+      const includeFields = ['userSubscribedTo', 'subscribedToUser'];
+
+      for (const field of includeFields) {
+        include[field] = fields[field] !== undefined;
+      }
+
+      const users = await prisma.user.findMany({ include });
+
+        users.forEach((user) => {
+          loaders.userDataLoader.prime(user.id, user);
+        });
+
       return users;
     },
   },
