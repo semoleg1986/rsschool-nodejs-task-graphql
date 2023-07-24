c647d9a57045d18a19086c2459e71d93e66c9cd9
Oleg Semenov <semoleg1986@gmail.com>
2023-07-24 16:35:35 +0300

    feat: add dataloader

---
+++ src/routes/graphql/dataloaders/memberDataloader.ts
@@ -0,0 +1,15 @@
+import DataLoader from "dataloader";
+import { MemberType, PrismaClient} from '@prisma/client';
+
+export const memberLoader = (prisma: PrismaClient) => {
+    return new DataLoader(async (keys: readonly string[]) => {
+      const members: MemberType[] = await prisma.memberType.findMany({
+        where: {
+          id: { in: keys as string[] },
+        },
+      });
+  
+      const sortedMemberType = keys.map((id) => members.find((member) => member.id === id));
+      return sortedMemberType;
+    });
+  };
